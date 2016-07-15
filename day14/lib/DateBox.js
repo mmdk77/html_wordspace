2dbb465b68550fa59cc0ec1f41260db5e74caa16
mmdk77 <mmdk77@gmail.com>
2016-07-15 17:47:12 +0900

    15일차 마무리

--- day14/lib/DateBox.js
+++ day14/lib/DateBox.js
@@ -1,21 +1,80 @@
-var DateBox = function(stage,wid,hei,txt){
-	this.stage=stage;
+var DateBox = function(stage, width, height, text){
 	this.div;
-	this.width=wid;
-	this.height=hei;
-	this.text=txt;
-	
-	this.init=function(){
-		this.div=document.createElement("div");
-		this.div.style.width=this.width+"px";
-		this.div.style.height=this.height+"px";
-		this.div.style.border="1px solid #ccffff";
-		this.div.style.float="left";
+	this.stage=stage;
+	this.width=width;
+	this.height=height;
+	this.text=text;
+	var me=this;
+	this.area;//undefined
+	this.title; //날짜를 담을 div
+	this.content; //입력폼과 내용을 담을 div
+	this.input;
 
-		this.div.innerText=this.text;
+	this.init = function(){
+		this.div = document.createElement("div");
+		this.div.style.width = this.width+"px";
+		this.div.style.height = this.height+"px";
+		this.div.style.border = "1px solid blue";
+		//this.div.innerText = this.text;
+		this.div.style.float = "left";
+		
+		this.title=document.createElement("div");	
+		this.title.style.width=100+"%";
+		this.title.style.height=25+"px";
+		this.title.innerText=this.text;
+		this.div.appendChild(this.title);//날짜 부착!!
+
+		this.content=document.createElement("div");	
+		this.content.style.width=100+"%";
+		this.content.style.height=75+"px";
+		this.content.style.overflow="scroll";
+		this.content.fontSize="9pt";
+		this.div.appendChild(this.content);//날짜 부착!!
+
+		//div에 대한 이벤트처리 
+		this.div.addEventListener("click", function(){
+			if(me.area==undefined){//생성된 적이 없다면...
+				me.createForm();
+			}
+		});
 
 		this.stage.appendChild(this.div);
-				
 	}
 
+	//클릭시 TextArea 와 Button을 등장시키기!!~
+	this.createForm=function(){
+		this.area=document.createElement("textarea");
+		this.area.style.width=95+"%";
+		this.area.style.height=45+"px";
+		this.area.style.background="#33ffff";
+		
+		this.input=document.createElement("input");
+		this.input.type="button";
+		this.input.value="등록";
+		
+		this.input.addEventListener("click", function(){
+			me.showContent();
+		});
+
+		this.content.appendChild(this.area);
+		this.content.appendChild(this.input);
+	}
+
+	this.showContent=function(){
+		var str=this.area.value;
+		this.content.removeChild(this.area);
+		this.content.removeChild(this.input);
+		this.content.innerText=str;
+
+		
+	}
+
+
 }
+
+
+
+
+
+
+
---
+++ oracle_study/post.ctl
@@ -0,0 +1,7 @@
+load data
+infile 'zipcode.csv'
+insert into table post
+fields terminated by ','
+(zipcode,sido,gugun,dong,bunji,seq)
+
+
